e61014ae49f6b6b1c3cb66abb1ba500bb3807fc7
abaid2694 <reha1_15@uni.worc.ac.uk>
2016-10-11 11:34:05 +0100

    chang the name

--- www/js/index.js
+++ www/js/index.js
@@ -1,12 +1,15 @@
+//these are the variable of the programm
 var paused_count =0;
 var resumed_count = 0;
 var launched_count = 0;
 
+//this function check the first command if the device is ready or not
     function onLoad() {
         document.addEventListener("deviceready", onDeviceReady, false);
 		console.log("device ready");
     }
 	
+//this function updates the display any changes made
 	function updateDisplay() {
 		$("#launched").text("Application launched: " + launched_count);
 		$("#resumed").text("Application paused: " + paused_count);
